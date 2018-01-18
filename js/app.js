7db1f6c292c8332eced439ede8224acf445c0fea
ethan375 <ethanebel1@gmail.com>
2018-01-18 02:56:41 -0600

    paddles created

--- js/app.js
+++ js/app.js
@@ -14,14 +14,26 @@ let ballR = 10;
 let animate = ()=>{
   requestAnimationFrame(animate);
   ctx.clearRect(0,0,innerWidth, innerHeight);
-
   ballX += 10;
-
+  paddle1();
+  paddle2();
   ctx.beginPath();
   ctx.arc(ballX, ballY, ballR, 0, Math.PI * 2, false);
   ctx.strokeStyle = 'white';
   ctx.stroke();
+}
 
+let paddle1 = ()=>{
+  ctx.fillStyle = 'blue';
+  ctx.fillRect(0, 100, 10, 100)
+}
 
+let paddle2 = ()=>{
+  ctx.fillStyle = 'blue';
+  ctx.fillRect(890, 100, 10, 100)
 }
 animate();
+
+
+
+
