2f43c9de50419517c8a31915a10c1703ff1a1298
ethan375 <ethanebel1@gmail.com>
2018-01-18 02:36:27 -0600

    ball drawn on canvas

--- js/app.js
+++ js/app.js
@@ -2,3 +2,12 @@ console.log("Js file linked and working");
 
 const canvas = document.getElementById('gameCanvas');
 const ctx = canvas.getContext('2d');
+
+let draw = ()=>{
+  ctx.beginPath();
+  ctx.arc(100, 100, 30, 0, Math.PI * 2, false);
+  ctx.strokeStyle = 'white';
+  ctx.stroke();
+}
+
+draw();
