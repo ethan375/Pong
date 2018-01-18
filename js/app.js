8a4e2a3c0be2f02a19589e8580f792f75e4432e6
ethan375 <ethanebel1@gmail.com>
2018-01-18 03:09:03 -0600

    added up and down controls to both paddles

--- js/app.js
+++ js/app.js
@@ -6,7 +6,8 @@ const ctx = canvas.getContext('2d');
 let ballX = 100;
 let ballY = 100;
 let ballR = 10;
-
+let paddle1Y = 100;
+let paddle2Y = 100
 
 
 
@@ -25,13 +26,29 @@ let animate = ()=>{
 
 let paddle1 = ()=>{
   ctx.fillStyle = 'blue';
-  ctx.fillRect(0, 100, 10, 100)
+  ctx.fillRect(0, paddle1Y, 10, 100)
 }
 
 let paddle2 = ()=>{
   ctx.fillStyle = 'blue';
-  ctx.fillRect(890, 100, 10, 100)
+  ctx.fillRect(890, paddle2Y, 10, 100)
 }
+
+
+document.addEventListener('keydown', (event)=>{
+  let key = event.which;
+  if(key == 38){
+    paddle2Y -= 20;
+  }else if(key == 40){
+    paddle2Y +=20
+  }else if(key == 65){
+    paddle1Y -=20
+  }else if(key == 90){
+    paddle1Y +=20
+  }
+  console.log(key)
+});
+
 animate();
 
 
