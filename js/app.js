25422d13030f0e691f61167dd1c517187cf984e5
ethan375 <ethanebel1@gmail.com>
2018-01-18 02:43:35 -0600

    ball moving to the right

--- js/app.js
+++ js/app.js
@@ -3,11 +3,24 @@ console.log("Js file linked and working");
 const canvas = document.getElementById('gameCanvas');
 const ctx = canvas.getContext('2d');
 
+let ballX = 100;
+let ballY = 100;
+let ballR = 10;
+
+window.onload = ()=>{
+  setInterval(draw, 500);
+}
+
+
+
 let draw = ()=>{
   ctx.beginPath();
-  ctx.arc(100, 100, 30, 0, Math.PI * 2, false);
+  ctx.arc(ballX, ballY, ballR, 0, Math.PI * 2, false);
   ctx.strokeStyle = 'white';
   ctx.stroke();
+
+  ballX += 10;
+
 }
 
 draw();
